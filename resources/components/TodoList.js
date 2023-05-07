77bdebb0c9b65900a47f7ae3cf5c1d10c8186913
badalharit <badalharitmrt@gmail.com>
2023-05-07 12:08:23 +0530

    Adding `To Do List` React UI

---
+++ resources/components/TodoList.js
@@ -0,0 +1,157 @@
+function App() {
+
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "App" }, /*#__PURE__*/
+      React.createElement(React.Fragment, null, /*#__PURE__*/
+        React.createElement(TodoList, null)), /*#__PURE__*/
+      React.createElement(Foobar, null)));
+}
+
+function TodoList() {
+  const generateID = () => {
+    let result = "";
+    let input_length = 5;
+    let chars =
+      "[@678^#(ABC,F3qr.sIJKN_+}{:OPQRghi)jDEklm:~noGH=2pL*$Mtuvwx<STU1>5VW`XYZa4bcd&efyz09]";
+    for (var i = 0; i < input_length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  };
+  const sortList = (a, b) => {
+    return a.isCompleted > b.isCompleted ?
+      1 :
+      b.isCompleted > a.isCompleted ?
+        -1 :
+        0;
+  };
+  const [todoList, setTodoList] = React.useState(
+    [
+      { id: 1, taskName: "dummy text 1", isCompleted: false },
+      { id: 2, taskName: "dummy text 2", isCompleted: false },
+      { id: 3, taskName: "dummy text 3", isCompleted: true }].
+      sort(sortList));
+
+  const [newTask, setNewTask] = React.useState("");
+
+  const inputRef = React.useRef(null);
+  const btnRef = React.useRef(null);
+
+  const handleChange = e => {
+    e.preventDefault();
+    setNewTask(e.target.value);
+    console.log(newTask);
+  };
+  React.useEffect(() => {
+    const listener = event => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        btnRef.current.click();
+      }
+    };
+    inputRef.current.addEventListener("keydown", listener);
+    return () => {
+      inputRef.current.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  const handleDelete = id => {
+    setTodoList(todoList.filter(list => list.id !== id));
+  };
+
+  const handleEdit = content => {
+    setNewTask(content.taskName);
+    inputRef.current.value = content.taskName;
+    inputRef.current.focus();
+
+    setTodoList(todoList.filter(list => list.id !== content.id));
+  };
+  const handleDone = status => {
+    status.isCompleted = true;
+    setTodoList([...todoList.sort(sortList)]);
+  };
+  const addTask = () => {
+    if (inputRef.current.value !== "") {
+      const task = {
+        id: todoList.length === 0 ? 1 : generateID(),
+        taskName: newTask,
+        isCompleted: false
+      };
+
+      setTodoList([...todoList, task].sort(sortList));
+      setNewTask("");
+      inputRef.current.value = "";
+      inputRef.current.focus();
+    } else {
+      alert(customeAlert());
+    }
+  };
+  const customeAlert = () => {
+    let alertMsgs = [
+      "Please type in something",
+      "input can't be empty",
+      "Invalid input",
+      "something went wrong"];
+
+    let msgIndex = Math.floor(Math.random() * alertMsgs.length);
+    let randomMsg = alertMsgs[msgIndex];
+
+    return randomMsg;
+  };
+
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "todolist" }, /*#__PURE__*/
+      React.createElement("div", { className: "title" }, /*#__PURE__*/
+        React.createElement("h1", null, "TODO APP")), /*#__PURE__*/
+
+      React.createElement("div", { className: "addTask" }, /*#__PURE__*/
+        React.createElement("input", {
+          ref: inputRef,
+          type: "text",
+          onChange: handleChange,
+          placeholder: "Add a task........"
+        }), /*#__PURE__*/
+
+        React.createElement("button", { ref: btnRef, onClick: addTask, className: "addtask-btn" }, "Add Task")), /*#__PURE__*/
+      React.createElement("div", { className: "lists" },
+        todoList.length > 0 ? /*#__PURE__*/
+          React.createElement(React.Fragment, null,
+            todoList.map((list, id) => /*#__PURE__*/
+              React.createElement("div", {
+                key: id,
+                className: `list ${list.isCompleted ? "completed" : ""}`
+              }, /*#__PURE__*/
+
+                React.createElement("p", null, " ", list.taskName), /*#__PURE__*/
+                React.createElement("div", { className: "span-btns" }, /*#__PURE__*/
+                  React.createElement("span", {
+                    style: { opacity: list.isCompleted ? "0" : "1" },
+                    onClick: () => handleDone(list),
+                    title: "completed"
+                  }, "\u2713"), /*#__PURE__*/
+
+
+
+                  React.createElement("span", {
+                    className: "delete-btn",
+                    onClick: () => handleDelete(list.id),
+                    title: "delete"
+                  }, "X"), /*#__PURE__*/
+
+
+
+                  React.createElement("span", {
+                    className: "edit-btn",
+                    onClick: () => handleEdit(list),
+                    title: "edit"
+                  }, "\u21BB"))))) : /*#__PURE__*/
+          React.createElement("h1", null, "Nothing To see here"))));
+}
+
+
+const Foobar = () => {
+  return /*#__PURE__*/(
+    React.createElement("div", { id: "foobar" }, "Made with \uD83D\uDC99 Liquid | ", new Date().getFullYear()));
+
+};
+// Tells React to attach the HelloWorld component to the 'root' HTML div
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
---
+++ resources/views/todolist.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Functional React Todo List App</title>
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"> -->
+<link rel="stylesheet" href="../resources/css/TodoList.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div id="root"></div>
+<!-- partial -->
+  <script src='https://unpkg.com/react@18/umd/react.production.min.js'></script>
+<script src='https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'></script><script  src="../resources/components/TodoList.js"></script>
+
+</body>
+</html>
