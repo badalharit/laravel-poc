3a905fdb78424ae29c5465fa0e08207bb536500d
badalharit <badalharitmrt@gmail.com>
2023-05-07 18:23:27 +0530

    Added Comments

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 /**
- * API to hit: http://localhost/laravel-poc/public/api/addnewtask?taskName={{Hello World}}
+ * API to hit: http://localhost/laravel-poc/public/api/addnewtask?taskName={"taskName"}&api_token={"api_token"}
  */
 Route::middleware('ToDoOps')->post('/addnewtask', [ToDoController::class,'addNewTask']);
 
