0693d16bdf257c6b0c997847ca737d6655b4efa8
badalharit <badalharitmrt@gmail.com>
2023-05-07 12:14:47 +0530

    Optimized the code architecture

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>CodePen - Functional React Todo List App</title>
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"> -->
-<link rel="stylesheet" href="../resources/css/TodoList.css">
+<link rel="stylesheet" href="{{ asset('css/TodoList.css') }}">
 
 </head>
 <body>
@@ -12,7 +12,7 @@
 <div id="root"></div>
 <!-- partial -->
   <script src='https://unpkg.com/react@18/umd/react.production.min.js'></script>
-<script src='https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'></script><script  src="../resources/components/TodoList.js"></script>
+<script src='https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'></script><script  src="{{ asset('components/TodoList.js') }}"></script>
 
 </body>
 </html>
