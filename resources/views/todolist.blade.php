6802b319eb7b5ce6f83f986c692a41244a88f120
badalharit <badalharitmrt@gmail.com>
2023-05-09 20:48:34 +0530

    Enabled `normalize.css` that makes browsers render all elements more consistently and in line with modern standards.

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Functional React Todo List App</title>
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"> -->
+  <title>ᑭᖇIᗩᒪ - Personal Productivity Tool</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel="stylesheet" href="{{ asset('css/TodoList.css') }}">
 
 </head>
